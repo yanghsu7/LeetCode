bfeabd1ffe9a05e7af5661dde286d32e1f6f22f6
yanghsu7 <mozzie.hsu@gmail.com>
2016-01-06 21:55:46 +0800

    Create TwoSum.cpp

---
+++ TwoSum.cpp
@@ -0,0 +1,22 @@
+# LeetCode
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> a;
+        int index1 =0;
+        int index2 =0;
+        //O(n^2)
+       for(int i =0; i<nums.size();i++)
+            for(int j =i+1; j<nums.size();j++)
+                if((nums[i]+nums[j]==target)){
+                    index1= i+1;
+                    index2 =j+1;
+                    cout<<"index1="<<index1<<", index2="<<index2<<endl;
+                    int b[]={index1,index2};
+                    a.assign(b,b+2);
+                }
+        return a;
+    }
+
+};
